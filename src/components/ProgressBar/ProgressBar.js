0a61b6e0a6421ce12f72206ab8355d1eaf5afe46
0815Strohhut <44547548+0815Strohhut@users.noreply.github.com>
2021-04-11 12:07:54 +0200

    Part 1: Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,54 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+const SIZES = {
+  small: {
+    "--bar": "8px"
+  },
+  medium: {
+    "--bar": "12px"
+  },
+  large: {
+    "--bar": "24px",
+    "--barPadding": "4px"
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const boundedValue = Math.max(0, Math.min(100, value));
+  const sizeStyles = SIZES[size];
+  const styles = SIZES[size];
+  return <Wrapper style={styles}>
+    <BarContainer>
+      <CurrentBar value={boundedValue} aria-valuenow={boundedValue} aria-valuemin="0" aria-valuemax="100">
+      </CurrentBar>
+    </BarContainer>
+  </Wrapper>;
 };
+
+const BarContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const CurrentBar = styled.div`
+  width: ${p => p.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+`;
+
+const Wrapper = styled.div`
+  width: 150px;
+  height: var(--bar);
+  padding: var(--barPadding);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35)
+`;
+
 
 export default ProgressBar;
