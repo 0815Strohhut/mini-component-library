4bae8a2c68cd1cb62279269e78775b9302aa8a12
0815Strohhut <44547548+0815Strohhut@users.noreply.github.com>
2021-04-11 16:19:25 +0200

    Textfield: Missing the offset for the border and the hover effect

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,7 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +14,59 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const height = size === "small" ? 24 : 36;
+  return <Wrapper width={width} height={height}>
+    <IconWrapper>
+      <Icon id={icon} size={height - 8} strokeWidth={size === "small" ? 1 : 2}></Icon>
+      <VisuallyHidden>
+        {label}
+      </VisuallyHidden>
+    </IconWrapper>
+    <Input placeholder={placeholder} size={size}></Input>
+
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: ${p => p.width}px;
+  padding-left: ${p => p.height + 12}px;
+  padding-top: ${p => (p.height - 12) / 3 - 2}px;
+  height: ${p => p.height}px;
+  border: 2px solid transparent;
+  border-radius: 2px;
+  border-bottom: 1px solid ${COLORS.black};
+
+  &:focus-within {
+    border: 2px solid #4374CB;
+    border-radius: 2px;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  font-size: ${p => (p.size === "small" ? 14 : 18) / 16 + "rem"};
+  font-weight: 700;
+  color: ${COLORS.gray700};
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 4px;
+`
 
 export default IconInput;
